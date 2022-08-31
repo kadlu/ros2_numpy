0c1e30ea37655ced8e4dfa30fad081ac09994b68
kadlu <50589471+kadlu@users.noreply.github.com>
2022-08-31 11:20:21 +0200

    Create setup.py
    for installing as a regular python module

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='ros2_numpy',
+      version='0.1',
+      packages=['ros2_numpy',
+                'ros2_numpy.geometry',
+                'ros2_nunpy.image',
+                'ros2_numpy.occupancy_grid',
+                'ros2_numpy.point_cloud2',
+                'ros2_numpy.registry])
